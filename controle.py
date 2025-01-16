22924492da6642c8c46c897a234e4f9597be0383
Diogo Oliveira <168039819+Diog9o@users.noreply.github.com>
2025-01-16 08:26:31 -0300

    Add files via upload

---
+++ controle.py
@@ -0,0 +1,26 @@
+from PyQt5 import uic, QtWidgets
+
+def principal():
+
+    salario = formulario.txtSalario.text()
+    salario = float(salario)
+    desconto = formulario.txtDescontos.text()
+    desconto = float(desconto)
+    resultado = (-salario /100 * desconto) + salario
+    fgtsMensal = salario/100*8
+    fgtsAnual = (salario/100*8)*12
+
+
+
+    formulario.lblResultado.setText(str(f'R${resultado:.2f}'))
+    formulario.lblFgtsMensal.setText(str(f'R${fgtsMensal:.2f}'))
+    formulario.lblFgtsAnual.setText(str(f'R${fgtsAnual:.2f}'))
+
+
+
+app = QtWidgets.QApplication([])
+formulario = uic.loadUi('tela.ui')
+formulario.btnCalcular.clicked.connect(principal)
+
+formulario.show()
+app.exec()
